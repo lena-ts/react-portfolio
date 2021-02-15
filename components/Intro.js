4e1757279f05c401a95650c4b72c4ed4fcb89fc0
Lena <justcodedevs@gmail.com>
2021-02-15 12:51:20 +0300

    Text update

--- components/Intro.js
+++ components/Intro.js
@@ -22,7 +22,7 @@ const Intro = () => {
                 <div>
                     <div className="l-text">
                         {/*Привет. Я Лена,<br></br>дизайнер и фронтенд<br></br>разработчик из Минска, Беларусь.*/}
-                        Hi! I'm Lena,<br></br>UI/UX designer & front-end<br></br>developer.<br></br>
+                        Hi! I'm Lena,<br></br>UI/UX designer & front-end<br></br>developer<br></br>
                     </div>
                     <p className="upper-base-text">Projects I've been involved with are Apps Design, ReactJS, React Native development, Magento, Prestashop design and front-end development
                         {/*<br></br><strong>React.js</strong>, <strong>Next.js</strong> apps development*/}
