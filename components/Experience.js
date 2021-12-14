b82c0516f526dfdfc66cdcde389c05e4215e1d44
Lena <tsybulenko.helen@gmail.com>
2021-12-14 16:23:40 +0300

    experience upd

--- components/Experience.js
+++ components/Experience.js
@@ -8,8 +8,12 @@ const Experience = () => {
                 <div className="uppercase-title">My Experience</div>
                     <ul>
                         <li>
+                            <div className="m-text">Designer at Yandex</div>
+                            <div className={styles.periods_text}>July 2021 — Present</div>
+                        </li>
+                        <li>
                             <div className="m-text">Designer Intern at Yandex</div>
-                            <div className={styles.periods_text}>March 2021 — Present</div>
+                            <div className={styles.periods_text}>March 2021 — July 2021</div>
                         </li>
                         <li>
                             <div className="m-text">Mobile Apps Designer</div>
