be45cb7617a17ed8c078e8d2a6c309041d8de087
Lena <justcodedevs@gmail.com>
2023-11-22 10:12:26 +0300

    upd

--- data/Me.js
+++ data/Me.js
@@ -1,6 +1,6 @@
 const Me = {
     hello: "Hi, I' Lena",
-    who: "I'm a product designer. I specialize in creating design for web and mobile apps, and also have experience in front-end development. Worked in Yandex and eCommerce development studio. I also have personal projects - mobile applications in the productivity category",
+    who: "I'm a product designer. I specialize in creating design for web and mobile apps, and also have experience in front-end development. Worked at bigtech company Yandex and eCommerce development studio.",
     hello_RU: "Привет, я Лена",
     who_RU: "Я дизайнер продукта. Специализируюсь на создании дизайна для веба и мобильных приложений. Имею опыт фронт-енд разработки, поэтому легко договариваюсь с разработчиками. Работала в Яндексе и студии eCommerce разработки. Также у меня есть личные проекты - мобильные приложения в категории продуктивность."
 }
