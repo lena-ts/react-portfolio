c0e2c137b9517009091d60903802e50789b90731
Lena <justcodedevs@gmail.com>
2025-07-21 15:32:01 +0300

    texts update

--- data/ExpericenceData.js
+++ data/ExpericenceData.js
@@ -4,28 +4,28 @@ const ExperienceData = {
 
     title_organic: "Senior Product Designer at Organic Fit",
     ru_title_organic: "Дизайнер продукта в Organic Apps",
-    date_organic: "February 2024 - Now",
-    years_organic: "7 month",
+    date_organic: "February 2024 - June 2025",
+    years_organic: "1 year 3 months",
 
     title_yandex: "Product Designer at Yandex",
     ru_title_yandex: "Дизайнер продукта в Яндексе",
     date_yandex: "March 2021 — June 2023",
     ru_date_yandex: "Март 2021 - Июнь 2023",
-    years_yandex: "2 years 3 month",
+    years_yandex: "2 years 3 months",
     ru_years_yandex: " 2 года",
 
     title_apps: "Mobile Apps Designer",
     ru_title_apps: "Дизайнер мобильных приложений",
     date_apps: "November 2019 — Present",
     ru_date_apps: "Ноябрь 2019 — Сейчас",
-    years_apps: "4 years",
+    years_apps: "5 years",
     ru_years_apps: "4 года",
 
     title_belvg: <div>Web Designer,<br></br>front-end developer at BelVG</div>,
     ru_title_belvg: <div>Веб-дизайнер,<br></br>фронт-енд разработчик в BelVG</div>,
     date_belvg: "August 2011 — November 2019",
     ru_date_belvg: "Август 2011 — Ноябрь 2019",
-    years_belvg: "8 years ",
+    years_belvg: "8 years",
     ru_years_belvg: "8 лет",
 }
 
