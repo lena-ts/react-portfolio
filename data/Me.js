9edf2513081bc09d2b7f2c99ed900f42795fd3be
Lena <justcodedevs@gmail.com>
2025-07-21 15:23:55 +0300

    texts update

--- data/Me.js
+++ data/Me.js
@@ -1,6 +1,6 @@
 const Me = {
     hello: "Lena Tsybulenko",
-    who: "Product Designer. I work in digital design industry for more than 10 years, 3 of which I have been a product designer. I got an experience in web and mobile interfaces, front-end development, illustrations and animations. I am currently working on the mobile fitness startup Organic Fit as a product designer.",
+    who: "I work in digital design industry for more than 10 years, 5 of which I have been a product designer. I got an experience in mobile and web interfaces, front-end development, illustrations and animations.",
     hello_RU: "Привет, я Лена",
     who_RU: "Я дизайнер продукта. Специализируюсь на создании дизайна для веба и мобильных приложений. Имею опыт фронт-енд разработки, поэтому легко договариваюсь с разработчиками. Работала в Яндексе и студии eCommerce разработки."
 }
